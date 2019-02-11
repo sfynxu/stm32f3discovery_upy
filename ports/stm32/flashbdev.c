613789d9a77c8a94e475779e8f1f3d4eca795ade
romeo <badea_romeo@yahoo.com>
2019-02-11 21:03:00 +0200

    Solved CCM

--- ports/stm32/flashbdev.c
+++ ports/stm32/flashbdev.c
@@ -53,6 +53,10 @@
 #define FLASH_MEM_SEG2_NUM_BLOCKS (128) // sector 11: 128k
 #endif
 
+#elif defined(STM32F303xC) 
+#define CACHE_MEM_START_ADDR (0x10000000) // CCM data RAM, 8k
+#define FLASH_SECTOR_SIZE_MAX (0x2000) //8k max, size of CCM
+
 #elif defined(STM32F401xE) || defined(STM32F411xE) || defined(STM32F446xx)
 
 STATIC byte flash_cache_mem[0x4000] __attribute__((aligned(4))); // 16k
